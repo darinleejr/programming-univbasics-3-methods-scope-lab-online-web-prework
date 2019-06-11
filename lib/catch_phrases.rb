ac5b98740fab1ca7be6e4bfe8018321af7d095a5
Darin Webster <darinleenavy@gmail.com>
2019-06-11 18:19:09 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -6,19 +6,19 @@ describe "mario" do
 end
 
 describe "toadstool" do
- it "puts out 'Thank You Mario! But Our Princess Is In Another Castle!'" do
+ it puts "Thank You Mario! But Our Princess Is In Another Castle!" do
    expect{toadstool}.to output("Thank You Mario! But Our Princess Is In Another Castle!\n").to_stdout
  end
 end
 
 describe "link" do
-  it "puts out 'It's Dangerous To Go Alone! Take This.'" do
+  it puts "It's Dangerous To Go Alone! Take This." do
     expect{link}.to output("It's Dangerous To Go Alone! Take This.\n").to_stdout
   end
 end
   
 describe "any_phrase" do
-  it "takes in an argument and puts out the catch phrase" do
+  it puts "takes in an argument and puts out the catch phrase." do
     phrase = "Do A Barrel Roll!"
     expect{any_phrase(phrase)}.to output("Do A Barrel Roll!\n").to_stdout
   end
